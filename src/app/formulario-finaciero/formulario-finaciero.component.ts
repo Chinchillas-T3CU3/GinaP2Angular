31c0222884b9596b9b8066bb89a8ec3b6892cb88
Chinchillas-T3CU3 <tecuechinchillas@gmail.com>
2025-04-23 08:18:10 -0600

    Arrenque del proyecto

---
+++ src/app/formulario-finaciero/formulario-finaciero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-finaciero',
+  standalone:true,
+  imports: [],
+  templateUrl: './formulario-finaciero.component.html',
+  styleUrl: './formulario-finaciero.component.css'
+})
+export class FormularioFinacieroComponent {
+
+}
