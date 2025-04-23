74f4fb8eedd2bd6c4afe6962c4e0f140fcb41c06
Vicente <vicente2.0luis@gmail.com>
2025-04-23 14:15:28 -0600

    listo principal

---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../vistas/navbar/navbar.component';
+import { HeaderComponent } from '../vistas/header/header.component';
+import { P1Component } from '../vistas/principalComponentes/p1/p1.component';
+import { P2Component } from '../vistas/principalComponentes/p2/p2.component';
+import { P3Component } from '../vistas/principalComponentes/p3/p3.component';
+
+@Component({
+  selector: 'app-principal',
+  imports: [NavbarComponent,HeaderComponent,P1Component,P2Component,P3Component],
+  templateUrl: './principal.component.html',
+  styleUrl: './principal.component.css'
+})
+export class PrincipalComponent {
+
+}
